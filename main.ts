194b2521cc523e3101616c8c92dd3339f3335965
AngadSC <104170661+AngadSC@users.noreply.github.com>
2022-06-07 21:14:22 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,14 +9,21 @@ basic.forever(function () {
     DigitalPin.P2,
     PingUnit.Centimeters
     )
+    serial.writeLine("" + (sonar.ping(
+    DigitalPin.P12,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )))
     if (distance > 15) {
         pins.analogWritePin(AnalogPin.P14, 1023)
     } else if (distance < 15 && distance > 10) {
         pins.analogWritePin(AnalogPin.P15, 1023)
     } else if (distance < 10) {
         pins.analogWritePin(AnalogPin.P13, 1023)
-        pins.analogWritePin(AnalogPin.P0, 1023)
-        music.playMelody("C B F - D G A C ", 51)
+    }
+})
+basic.forever(function () {
+    if (distance < 10) {
         basic.showLeds(`
             # . . . #
             . # . # .
@@ -24,5 +31,7 @@ basic.forever(function () {
             . # . # .
             # . . . #
             `)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        music.playMelody("C B F - D G A C ", 51)
     }
 })
