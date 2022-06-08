c02384da5c9ab7daebaa3f0c63fc5093fa2b0638
AngadSC <104170661+AngadSC@users.noreply.github.com>
2022-06-07 21:32:16 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 let distance = 0
+input.onPinReleased(TouchPin.P0, function () {
+	
+})
 function red () {
     pins.analogWritePin(AnalogPin.P14, 1023)
     pins.analogWritePin(AnalogPin.P15, 1023)
@@ -33,5 +36,12 @@ basic.forever(function () {
             `)
         pins.digitalWritePin(DigitalPin.P0, 1)
         music.playMelody("C B F - D G A C ", 51)
+    }
+})
+basic.forever(function () {
+    if (input.pinIsPressed(TouchPin.P1)) {
+        basic.clearScreen()
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        music.stopAllSounds()
     }
 })
